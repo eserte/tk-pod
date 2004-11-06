03eb0076fc768ab39f6a3c09933ff76a90b0b7cd
Slaven Rezic <slaven@rezic.de>
2004-11-06 23:43:18 +0000

    ups, ptked was not in background...

--- Pod/Text.pm
+++ Pod/Text.pm
@@ -26,7 +26,7 @@ use Tk::Pod::Util qw(is_in_path is_interactive detect_window_manager);
 use vars qw($VERSION @ISA @POD $IDX
 	    @tempfiles @gv_pids $terminal_fallback_warn_shown);
 
-$VERSION = sprintf("%d.%02d", q$Revision: 5.2 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 5.3 $ =~ /(\d+)\.(\d+)/);
 
 @ISA = qw(Tk::Frame Tk::Pod::SimpleBridge Tk::Pod::Cache);
 
@@ -261,7 +261,7 @@ sub edit
   }
  if ($^O eq 'MSWin32') # XXX what is right?
   {
-   system("ptked $path");
+   system("ptked $path &");
   }
  else
   {
