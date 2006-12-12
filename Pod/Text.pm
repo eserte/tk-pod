f853c0cb5a5e28b39cf551de9550c16765c024d6
Slaven Rezic <slaven@rezic.de>
2006-12-12 21:45:28 +0000

    title for context menu

--- Pod/Text.pm
+++ Pod/Text.pm
@@ -26,7 +26,7 @@ use Tk::Pod::Util qw(is_in_path is_interactive detect_window_manager start_brows
 use vars qw($VERSION @ISA @POD $IDX
 	    @tempfiles @gv_pids $terminal_fallback_warn_shown);
 
-$VERSION = sprintf("%d.%02d", q$Revision: 5.6 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 5.7 $ =~ /(\d+)\.(\d+)/);
 
 @ISA = qw(Tk::Frame Tk::Pod::SimpleBridge Tk::Pod::Cache);
 
@@ -406,7 +406,8 @@ sub Populate
     };
 
     my $m = $p->Menu
-	(-tearoff => $Tk::platform ne 'MSWin32',
+	(-title => "Tkpod",
+	 -tearoff => $Tk::platform ne 'MSWin32',
 	 -menuitems =>
 	 [
 	  [Button => 'Back',     -command => [$w, 'history_move', -1]],
