0db8e6357e28da090f76cc824013de6850bb9628
Slaven Rezic <slaven@rezic.de>
2008-08-16 20:39:47 +0000

    * 0.9939_53

--- Pod.pm
+++ Pod.pm
@@ -4,8 +4,8 @@ use Tk ();
 use Tk::Toplevel;
 
 use vars qw($VERSION $DIST_VERSION @ISA);
-$VERSION = sprintf("%d.%02d", q$Revision: 5.29 $ =~ /(\d+)\.(\d+)/);
-$DIST_VERSION = "0.9939_52";
+$VERSION = sprintf("%d.%02d", q$Revision: 5.30 $ =~ /(\d+)\.(\d+)/);
+$DIST_VERSION = "0.9939_53";
 
 @ISA = qw(Tk::Toplevel);
 
